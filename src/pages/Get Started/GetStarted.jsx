13cdd9d24a1f3a973ae0aad182de97adaaa8619c
archiesevillano <archie.sevillano29@gmail.com>
2023-07-23 22:06:12 +0800

    Added Responsiveness to btnGroup in Get Started Page

--- src/pages/Get Started/GetStarted.jsx
+++ src/pages/Get Started/GetStarted.jsx
@@ -6,13 +6,13 @@ const GetStarted = () => {
             <h1 className="getStarted__h1">Morse Code Translate</h1>
             <p className="getStarted__para2 p">Transform your ideas into trendy Morse code and send a mysterious message to your pals.</p>
             <div className="getStarted__btnGroup">
-                <div className="getStarted__btnContainer" role="button">
+                <div className="getStarted__btnContainer--translator" role="button">
                     <div className="getStarted__btnContainer__texts">
                         <p>Try for Free</p>
                         <p>Morse Translate</p>
                     </div>
                 </div>
-                <div className="getStarted__btnContainer" role="button">
+                <div className="getStarted__btnContainer--api" role="button">
                     <div className="getStarted__btnContainer__texts">
                         <p>Free and No API Key</p>
                         <p>{import.meta.env.VITE_APP_NAME} API for Free</p>
